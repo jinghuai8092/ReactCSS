58a9627bd2affff677b835bfc2796711a313de8e
景淮jinghuai <jinghuai8092@gmail.com>
2021-07-27 22:47:01 -0400

    React - transition -group

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//all in js
+import App2 from './app2'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App2 />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
